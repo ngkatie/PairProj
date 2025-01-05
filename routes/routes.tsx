19e6ef61819929f660122e14e66f64f7215b694a
Phillip Anerine <paner225@gmail.com>
2025-01-05 15:52:05 -0500

    Update routes

--- routes/routes.tsx
+++ routes/routes.tsx
@@ -1,13 +1,12 @@
 import { registerRoutes } from "./routeBuilder";
 
 export const routes = registerRoutes({
-	home: "/home",
-	about: "/about",
 	login: "/login",
+	users: {
+		profile: "/users/:id",
+	},
 	projects: {
-		list: "/projects/list",
 		search: "/projects/search",
 		project: "/projects/:id",
-		contributor: "/projects/:projectId/contributor/:contributorId",
 	},
 });
