1b688d2cbd796eebe28e5bb5b641b992c76cebd6
Winnie Yu <winyu9491@gmail.com>
2025-04-24 14:27:06 -0400

    updated page.tsx

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -34,6 +34,7 @@ export default function Page() {
     console.log(fileData);
   }
   return (
+    <Room roomId={roomId}>
     <div className="grid grid-cols-12 min-h-screen" suppressHydrationWarning>
         <div className="col-span-3 bg-gray-200 dark:bg-zinc-900 text-black dark:text-white w-full p-4 flex flex-col gap-6">
           <Header />
@@ -43,19 +44,18 @@ export default function Page() {
           </div>
           <ThemeToggle />
         </div>
-                <div className="col-span-6 bg-white dark:bg-black text-black dark:text-white w-full p-6">
-                  <Room roomId={roomId}>
-                  <div className="flex justify-between items-center p-4">
-                    <h1 className="text-xl font-bold">Live Editor</h1>
-                    <ShareButton roomId={roomId} />
-                  </div>
-                    <CollaborativeEditor data={fileData}/>
-                  </Room>
-                </div>
-                <div className="col-span-3 bg-gray-200 dark:bg-zinc-900 text-black dark:text-white w-full p-4 flex flex-col items-start gap-4">
-                  <h1 className="text-2xl font-bold">Active Users</h1>
-                  <Avatars />
-                </div>
-            </div>
+        <div className="col-span-6 bg-white dark:bg-black text-black dark:text-white w-full p-6">
+          <div className="flex justify-between items-center p-4">
+            <h1 className="text-xl font-bold">Live Editor</h1>
+            <ShareButton roomId={roomId} />
+          </div>
+            <CollaborativeEditor data={fileData}/>
+        </div>
+        <div className="col-span-3 bg-gray-200 dark:bg-zinc-900 text-black dark:text-white w-full p-4 flex flex-col items-start gap-4">
+          <h1 className="text-2xl font-bold">Active Users</h1>
+          <Avatars />
+        </div>
+    </div>
+    </Room>
   );
 }
